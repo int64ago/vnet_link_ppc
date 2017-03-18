5ad19d8a6fc8d196670742ca9613d094003fb2e8
Cody Chan <int64ago@gmail.com>
2017-03-18 16:17:49 +0800

    feat: update regex

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ PAC_URL=https://vnet.link/project/ppc.pac
 
 PAC=$(curl -s -L $PAC_URL)
 
-DOMAINS=$(echo $PAC | grep -oP '(?<=")(\..+?)+(?=")' | sed 's/^\./||/')
+DOMAINS=$(echo $PAC | grep -oP '(?<=")([\w\.]{1,}?)+(?=")' | sed 's/^\./||/')
 
 echo -e "[AutoProxy]\n$DOMAINS" | base64 > ppc.txt
 
